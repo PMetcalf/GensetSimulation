13949bae39dacf27ac0301b0126ccfc52fec2d2f
PMetcalf <paulmetcalf@hotmail.com>
2020-05-18 17:02:12 +0100

    Added genset simulator project.

---
+++ GensetSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GensetSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
