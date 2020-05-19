53e278021fe28ea7da789093331699eec93661f5
PMetcalf <paulmetcalf@hotmail.com>
2020-05-19 11:36:31 +0100

    Added geneset properties to class.

--- GensetSimulator/Models/Genset.cs
+++ GensetSimulator/Models/Genset.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace GensetSimulator.Models
 {
+    /// <summary>
+    /// Genset class holds properties and methods for a genset.
+    /// </summary>
+
     class Genset
     {
+        /// <summary>
+        /// Bool determines if genset is running.
+        /// </summary>
+        private bool isOn;
+        public bool IsOn { get => isOn; set => isOn = value; }
+
+        /// <summary>
+        /// Property stores genset generated power.
+        /// </summary>
+        private int gensetPower;
+        public int GensetPower { get => gensetPower; set => gensetPower = value; }
     }
 }
