7284a248a33ed977128cac7efa023ac8a6eaadb7
PMetcalf <paulmetcalf@hotmail.com>
2020-05-19 11:31:35 +0100

    Comment on Program.

--- GensetSimulator/Program.cs
+++ GensetSimulator/Program.cs
@@ -4,6 +4,11 @@ namespace GensetSimulator
 {
     class Program
     {
+        /// <summary>
+        /// This is the main executable class for the program.
+        /// </summary>
+        /// <param name="args"></param>
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
