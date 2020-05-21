047c3d515a6d029c0b5420c6c2a225ccf6f4429d
PMetcalf <paulmetcalf@hotmail.com>
2020-05-21 20:09:57 +0100

    Design change to Program class.

--- GensetSimulator/Program.cs
+++ GensetSimulator/Program.cs
@@ -9,18 +9,61 @@ namespace GensetSimulator
         /// This is the main executable class for the program.
         /// </summary>
         /// <param name="args"></param>
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting Genset Simulator.");
+            Console.Title = typeof(Program).Name;
+            // We will add some set-up stuff here later...
 
-            // Initialise new genset.
-            Genset genset = new Genset();
+            Run();
+        }
 
-            // [Initialise webservice connection].
 
-            // Run genset / data logging.
-            genset.StartGenset(10);
+        static void Run()
+        {
+            while (true)
+            {
+                var consoleInput = ReadFromConsole();
+                if (string.IsNullOrWhiteSpace(consoleInput)) continue;
+
+                try
+                {
+                    // Execute the command:
+                    string result = Execute(consoleInput);
+
+                    // Write out the result:
+                    WriteToConsole(result);
+                }
+                catch (Exception ex)
+                {
+                    // OOPS! Something went wrong - Write out the problem:
+                    WriteToConsole(ex.Message);
+                }
+            }
+        }
+
+
+        static string Execute(string command)
+        {
+            // We'll make this more interesting shortly:
+            return string.Format("Executed the {0} Command", command);
+        }
+
+
+        public static void WriteToConsole(string message = "")
+        {
+            if (message.Length > 0)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+
+        const string _readPrompt = "console> ";
+        public static string ReadFromConsole(string promptMessage = "")
+        {
+            // Show a prompt, and get input:
+            Console.Write(_readPrompt + promptMessage);
+            return Console.ReadLine();
         }
     }
 }
