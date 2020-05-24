556f3487b71f064b36072ae36e4de65286ad6c1c
PMetcalf <paulmetcalf@hotmail.com>
2020-05-24 16:51:29 +0100

    Added Models Folder and GensetData class to solution.

---
+++ SimulationWebservice/Models/GensetData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimulationWebservice.Models
+{
+    /// <summary>
+    /// Contains data model for genset system.
+    /// </summary>
+
+    public class GensetData
+    {
+        // Id is used to identify data element in storage.
+        public string Id { get; set; }
+
+        //
+    }
+}
