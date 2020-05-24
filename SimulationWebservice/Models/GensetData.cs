c213ccf1b6613365fe9c2a2794d414929b08f6ab
PMetcalf <paulmetcalf@hotmail.com>
2020-05-24 16:58:12 +0100

    Added JSON property tag.

--- SimulationWebservice/Models/GensetData.cs
+++ SimulationWebservice/Models/GensetData.cs
@@ -1,4 +1,6 @@
-﻿namespace SimulationWebservice.Models
+﻿using Newtonsoft.Json;
+
+namespace SimulationWebservice.Models
 {
     /// <summary>
     /// Contains data model for genset system.
@@ -7,6 +9,7 @@
     public class GensetData
     {
         // Id is used to identify data element in storage.
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
 
         //
