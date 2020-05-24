d4c4a2ff9f491b283b7bd0ca1074f19090b0c0d8
PMetcalf <paulmetcalf@hotmail.com>
2020-05-24 17:45:56 +0100

    Added constructor.

--- SimulationWebservice/Services/CosmosDBService.cs
+++ SimulationWebservice/Services/CosmosDBService.cs
@@ -1,6 +1,26 @@
-﻿namespace SimulationWebservice.Services
+﻿
+
+using Microsoft.Azure.Cosmos;
+
+namespace SimulationWebservice.Services
 {
-    public class CosmosDBService
+    /// <summary>
+    /// Connect and use Azure Cosmos DB, including CRUD operations.
+    /// </summary>
+
+    public class CosmosDBService : ICosmosDbService
     {
+        private Container container;
+
+        /// <summary>
+        /// Use client to connect place database connection in container.
+        /// </summary>
+        /// <param name="dbClient"></param>
+        /// <param name="dbName"></param>
+        /// <param name="containerName"></param>
+        public CosmosDBService(CosmosClient dbClient, string dbName, string containerName)
+        {
+            this.container = dbClient.GetContainer(dbName, containerName);
+        }
     }
 }
