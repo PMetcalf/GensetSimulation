b2b4d35f1e54f426947eafde08031242a96db509
PMetcalf <paulmetcalf@hotmail.com>
2020-05-24 17:29:13 +0100

    Removed unused modules.

--- SimulationWebservice/Services/CosmosDBService.cs
+++ SimulationWebservice/Services/CosmosDBService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SimulationWebservice.Services
+﻿namespace SimulationWebservice.Services
 {
     public class CosmosDBService
     {
