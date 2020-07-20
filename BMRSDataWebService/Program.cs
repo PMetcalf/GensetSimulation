e17a2b2394e39504415b509612764625a346931b
PMetcalf <paulmetcalf@hotmail.com>
2020-07-20 15:48:54 +0100

    getBMRSData wip

--- BMRSDataWebService/Program.cs
+++ BMRSDataWebService/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,5 +23,14 @@ namespace BMRSDataWebService
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        /// <summary>
+        /// Routine makes async call to BMRS web service using HTTP Client.
+        /// </summary>
+        /// <returns></returns>
+        public static async Task getBMRSData()
+        {
+            using var client = new HttpClient();
+        }
     }
 }
