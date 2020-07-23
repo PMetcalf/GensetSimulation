f764b4e73af5dd4341e88d3bac76958ecd95feea
PMetcalf <paulmetcalf@hotmail.com>
2020-07-23 15:40:59 +0100

    Added http client instances.

--- E_GridDataShunter/Program.cs
+++ E_GridDataShunter/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace E_GridDataShunter
 {
     class Program
     {
+        // Separate http clients used to (1) collect data and (2) send data to storage.
+        static HttpClient clientBMRSData = new HttpClient();
+        static HttpClient clientDataStore = new HttpClient();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // Initialise http client.
+
+            // Request data.
+
+            // Process data?
+
+            // Send data to database.
         }
+
+        ///
     }
 }
