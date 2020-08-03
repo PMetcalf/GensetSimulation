f70ddfcf65a078b3006c2697cd0d2520c1b6453f
PMetcalf <paulmetcalf@hotmail.com>
2020-08-03 14:45:35 +0100

    Removed unused method from program.

--- BMRSDataWebService/Program.cs
+++ BMRSDataWebService/Program.cs
@@ -15,8 +15,6 @@ namespace BMRSDataWebService
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-
-            //await getBMRSData();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -26,25 +24,5 @@ namespace BMRSDataWebService
                     webBuilder.UseStartup<Startup>();
                 });
 
-        /// <summary>
-        /// Routine makes async call to BMRS web service using HTTP Client.
-        /// </summary>
-        /// <returns></returns>
-        public static async Task getBMRSData()
-        {
-            using var client = new HttpClient();
-
-            // Setup http client
-            client.BaseAddress = new Uri("https://api.bmreports.com/BMRS/B1630/V1?");
-
-            // Call BMRS API
-            var response = await client.GetAsync("APIKey=ittvxvqico9tta1&SettlementDate=2020-06-25&Period=1&ServiceType=csv");
-
-            // Parse response
-            if (response.IsSuccessStatusCode)
-            {
-                string message = await response.Content.ReadAsStringAsync();
-            }
-        }
     }
 }
