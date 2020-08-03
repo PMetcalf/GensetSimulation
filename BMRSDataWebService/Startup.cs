ff4df81222a181c0a384354f065660bbf63236a9
PMetcalf <paulmetcalf@hotmail.com>
2020-08-03 15:55:03 +0100

    Initialise cosmos client outline

--- BMRSDataWebService/Startup.cs
+++ BMRSDataWebService/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BMRSDataWebService.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -46,6 +47,16 @@ namespace BMRSDataWebService
             {
                 endpoints.MapControllers();
             });
+        }
+
+        /// <summary>
+        /// Initialises Cosmos DB client connection and container.
+        /// </summary>
+        /// <param name="configurationSection"></param>
+        /// <returns></returns>
+        private static async Task<CosmosDBService> InitialiseCosmosClientInstanceAsync(IConfigurationSection configurationSection)
+        {
+
         }
     }
 }
