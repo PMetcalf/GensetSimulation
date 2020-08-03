3d4556b243238acc71bfe226878215fc0b20cdaa
PMetcalf <paulmetcalf@hotmail.com>
2020-08-03 15:29:39 +0100

    Added constructor and variable declarations.

--- BMRSDataWebService/Controllers/DataStoreController.cs
+++ BMRSDataWebService/Controllers/DataStoreController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BMRSDataWebService.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BMRSDataWebService.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
     [ApiController]
+    [Route("[controller]")]
     public class DataStoreController : ControllerBase
     {
+        private readonly ICosmosDbService cosmosDbService;
+
+        public DataStoreController(ICosmosDbService dbService)
+        {
+            cosmosDbService = dbService;
+        }
     }
 }
