cec29e2b4d9d96cd55924b121b287a732789d205
PMetcalf <paulmetcalf@hotmail.com>
2020-09-11 16:08:39 +0100

    Added config

--- data_analytics/src/data/cosmos_client_manager.py
+++ data_analytics/src/data/cosmos_client_manager.py
@@ -11,7 +11,10 @@ import azure.cosmos.exceptions as exceptions
 from azure.cosmos.partition_key import PartitionKey
 
 # Project Modules
-# import config
+import config
 
 # Constants
-
+HOST = config.settings['host']
+MASTER_KEY = config.settings['master_key']
+DATABASE_ID = config.settings['database_id']
+CONTAINER_ID = config.settings['container_id']
