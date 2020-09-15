26ab9aa3680a16a955afc4ad5ee7acc170a22af2
PMetcalf <paulmetcalf@hotmail.com>
2020-09-15 11:01:03 +0100

    convert to dict implementation

--- data_analytics/src/data/load_data.py
+++ data_analytics/src/data/load_data.py
@@ -9,8 +9,25 @@ This file supports load and save operations.
 
 # CONSTANTS
 
-def convert_dict_to_dataframe():
-    pass
+def convert_dict_to_dataframe(items_dict):
+    """Convert to Dataframe
+    ======================================
+    Converts dict of raw database items into dataframe.
+    
+    Args:
+        items_dict (dict) - Dict of items from database container.
+        
+    Returns:
+        df (dataframe) - Dataframe containing items from container list.
+    """
+    
+    # Convert directly into dataframe
+    df_items = pd.DataFrame(items_dict)
+
+    print("Converted to dataframe ...")
+
+    # Return dataframe
+    return df_items
 
 def save_local_dataframe():
     pass
