3b7304d2db644be617363136da6073295a61d18a
PMetcalf <paulmetcalf@hotmail.com>
2020-09-15 10:58:09 +0100

    load data outline

--- data_analytics/src/data/load_data.py
+++ data_analytics/src/data/load_data.py
@@ -1,6 +1,19 @@
 '''
 load_data
 
-This file supports loading project data into memory.
+This file supports load and save operations.
 
 '''
+
+# Module Importations
+
+# CONSTANTS
+
+def convert_dict_to_dataframe():
+    pass
+
+def save_local_dataframe():
+    pass
+
+def load_local_dataframe():
+    pass
