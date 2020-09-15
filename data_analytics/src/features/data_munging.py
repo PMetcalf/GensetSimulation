cb00f172235a2c43f351775992fd85361d5ec2ff
PMetcalf <paulmetcalf@hotmail.com>
2020-09-15 11:51:03 +0100

    Remove unused columns implementation

--- data_analytics/src/features/data_munging.py
+++ data_analytics/src/features/data_munging.py
@@ -8,11 +8,26 @@ This file supports data cleaning and preprocessing operations.
 # Module Importations
 import pandas as pd
 
-def remove_unused_columns():
-    pass
+def remove_unused_columns(df_original):
+    """Remove Unused Columns
+    ======================================
+    Removes unused / unecessary columns from base dataframe, returning a new dataframe.
+    
+    Args:
+        df_original (DataFrame) - Dataframe to have columns removed.
+        
+    Returns:
+        df_modified (DataFrame) - New dataframe to have columns removed.
+    """
 
-def add_generation_date():
-    pass
+   # Create copy of original dataframe
+    df_modified = df_original.copy()
 
-def add_generation_period():
-    pass
+    # Remove column names from dataframe
+    df_modified = df_modified.drop("_rid", axis = 1)
+    df_modified = df_modified.drop("_self", axis = 1)
+    df_modified = df_modified.drop("_etag", axis = 1)
+    df_modified = df_modified.drop("_attachments", axis = 1)
+    
+    # Return dataframe copy
+    return df_modified     
