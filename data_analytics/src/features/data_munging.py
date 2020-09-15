b9d73d2d1f03a3937090d95370033e0c3bb37b78
PMetcalf <paulmetcalf@hotmail.com>
2020-09-15 11:24:09 +0100

    Added data munging file and method outline

---
+++ data_analytics/src/features/data_munging.py
@@ -0,0 +1,18 @@
+'''
+data_munging
+
+This file supports data cleaning and preprocessing operations.
+
+'''
+
+# Module Importations
+import pandas as pd
+
+def remove_unused_columns():
+    pass
+
+def add_generation_date():
+    pass
+
+def add_generation_period():
+    pass
