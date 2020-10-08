2c21576fe99e2f67ff50eadf7fe82b13c1d948bc
Paul Metcalf <paulmetcalf@hotmail.com>
2020-10-08 16:38:58 +0100

    Directory tidy

--- data_aquisition/DataStoreController/Startup.cs
+++ data_aquisition/DataStoreController/Startup.cs
@@ -26,6 +26,9 @@ namespace DataStoreController
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSingleton<ICosmosDbService>(InitialiseCosmosClientInstanceAsync(
+                Configuration.GetSection("CosmosDb")).GetAwaiter().GetResult());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +49,37 @@ namespace DataStoreController
             {
                 endpoints.MapControllers();
             });
+        }
+
+        /// <summary>
+        /// Initialises Cosmos DB client connection and container.
+        /// </summary>
+        /// <param name="configurationSection"></param>
+        /// <returns></returns>
+        private static async Task<CosmosDBService> InitialiseCosmosClientInstanceAsync(IConfigurationSection configurationSection)
+        {
+            // Set connection parameters
+            string databaseName = configurationSection.GetSection("DatabaseName").Value;
+            string containerName = configurationSection.GetSection("ContainerName").Value;
+            string account = configurationSection.GetSection("Account").Value;
+            string key = configurationSection.GetSection("Key").Value;
+
+            // Build client and services
+            Microsoft.Azure.Cosmos.Fluent.CosmosClientBuilder clientBuilder =
+                new Microsoft.Azure.Cosmos.Fluent.CosmosClientBuilder(account, key);
+
+            Microsoft.Azure.Cosmos.CosmosClient client =
+                clientBuilder.WithConnectionModeDirect().Build();
+
+            CosmosDBService dBService = new CosmosDBService(client, databaseName, containerName);
+
+            // Initialise database, if required
+            Microsoft.Azure.Cosmos.DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+
+            // Build the database container
+            await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+
+            return dBService;
         }
     }
 }
