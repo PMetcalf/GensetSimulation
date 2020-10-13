7e53e307f9ff75d24f26041ebb4b3af4f8190f49
Paul Metcalf <paulmetcalf@hotmail.com>
2020-10-13 11:07:54 +0100

    Wip.

--- data_aquisition/DatabaseController/Startup.cs
+++ data_aquisition/DatabaseController/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DatabaseController.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace DatabaseController
 {
@@ -27,6 +21,9 @@ namespace DatabaseController
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSingleton<ICosmosDbService>(InitialiseCosmosClientInstanceAsync(
+                Configuration.GetSection("CosmosDb")).GetAwaiter().GetResult());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
