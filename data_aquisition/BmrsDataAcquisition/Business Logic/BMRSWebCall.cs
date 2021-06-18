c95160d612410ed908dab56ae8a1722c5598e787
PMetcalf <paulmetcalf@hotmail.com>
2021-06-18 16:08:30 +0100

    Added BMRS Web Call Class.

---
+++ data_aquisition/BmrsDataAcquisition/Business Logic/BMRSWebCall.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BmrsDataAcquisition.Business_Logic
+{
+    public class BMRSWebCall
+    {
+    }
+}
