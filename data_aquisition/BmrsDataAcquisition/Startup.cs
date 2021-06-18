913b24db0267dab28c92588f4aa5e38c55a069e3
PMetcalf <paulmetcalf@hotmail.com>
2021-06-18 16:14:27 +0100

    Registered hosted service in config.

--- data_aquisition/BmrsDataAcquisition/Startup.cs
+++ data_aquisition/BmrsDataAcquisition/Startup.cs
@@ -1,3 +1,4 @@
+using BmrsDataAcquisition.Business_Logic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,7 @@ namespace BmrsDataAcquisition
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHostedService<bmrsWebCallHostedService>();
             services.AddRazorPages();
         }
 
