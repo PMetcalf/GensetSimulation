f9c062aa8b52f140dd0dde3641df6c4d952469bd
PMetcalf <paulmetcalf@hotmail.com>
2021-06-18 16:11:06 +0100

    Added renamed bmrs web call class.

---
+++ data_aquisition/BmrsDataAcquisition/Business Logic/bmrsWebCallHostedService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BmrsDataAcquisition.Business_Logic
+{
+    public class bmrsWebCallHostedService : IHostedService
+    {
+    }
+}
